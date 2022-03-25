2d10f75f7d5c55dae2f6949e8e588b44b62a4649
Wedeueis Braz <wedeueisbraz@yahoo.com.br>
2022-03-25 13:47:42 -0300

    updated storage test

--- apps/gcp_storage_test/storage_test.py
+++ apps/gcp_storage_test/storage_test.py
@@ -2,7 +2,10 @@ import sys
 from os import path
 import json
 
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+PROJECT_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+)
+sys.path.append(PROJECT_ROOT)
 
 from dotenv import load_dotenv
 
@@ -10,14 +13,7 @@ load_dotenv()
 
 from src.utils.datasource import read_dataframe_from_bucket_file
 
-FILENAME = "DS-Meetup/titanic/data/raw/train.csv"
-BUCKET_NAME = "wedeueis-braz"
+FILENAME = "path-to-the-file"
+BUCKET_NAME = "<path-to_your-bucket>"
 
 print(read_dataframe_from_bucket_file(BUCKET_NAME, FILENAME, sep=","))
-
-
-with open("config/local/config.json") as file:
-    config = json.load(file)
-    user = config["database_username"]
-    password = config["database_password"]
-    print(user)
